5919b170e991912b60f3d6968747dfc3663f7290
Divansh Singh <divansh.singh@testingxperts.com>
2025-05-08 15:52:34 +0530

    converstions

---
+++ comparisons_conversions.js
@@ -0,0 +1,19 @@
+let x = "2" +1 ; 
+console.log(x); 
+console.log(typeof x);
+let y = 1 + "2";
+console.log(y); 
+console.log(typeof y);
+let t = "1" + 2 + 3 ;
+
+
+console.log(t);
+console.log(typeof t);
+
+
+let z = 1 + 2 + "3"; 
+console.log(z);
+console.log(typeof z);
+
+console.log(1 + 2 + "3" + 4 + 5); 
+console.log(1 + 2 + "3" + (4 + 5));
